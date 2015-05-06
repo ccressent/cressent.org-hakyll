a7b58ceb73fb93ade0527ff6008c91e8f0a7cad5
Cyril Cressent <cyril@cressent.org>
2015-05-06 17:25:42 +0100

    Add an update date that works
    Add a new field type, updatedField, that lets one put and updated date
    and time in an item's metadata.

--- src/website.hs
+++ src/website.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad (liftM)
+import Control.Monad (liftM, msum)
+import qualified Data.Map as M
 import Data.Monoid ((<>))
+import Data.Time.Clock (UTCTime)
+import Data.Time.Format (formatTime, parseTime)
 import Hakyll
+import System.Locale (TimeLocale, defaultTimeLocale)
 
 
 main :: IO ()
@@ -77,6 +81,7 @@ staticFiles = [ ".htaccess"
 articleCtx :: Context String
 articleCtx =
     dateField "date" "%B %e, %Y"
+ <> updatedField "updated" "%B %e, %Y"
  <> defaultContext
 
 
@@ -86,3 +91,33 @@ config = defaultConfiguration
     , ignoreFile        = const False
     , deployCommand     = "echo 'No deploy command specified' && exit 1"
     }
+
+
+-- An "updated" datetime field that can be explicitely put in the document's
+-- metadata.
+-- Thanks to http://david.sferruzza.fr/posts/2014-06-18-new-blog-with-hakyll.html
+updatedField :: String -> String -> Context a
+updatedField key format = field key $ \i -> do
+    time <- getUpdatedTime locale $ itemIdentifier i
+    return $ formatTime locale format time
+  where
+    locale = defaultTimeLocale
+
+getUpdatedTime :: MonadMetadata m => TimeLocale -> Identifier -> m UTCTime
+getUpdatedTime locale id' = do
+    metadata <- getMetadata id'
+    let tryField k fmt = M.lookup k metadata >>= parseTime' fmt
+    maybe empty' return $ msum [tryField "updated" fmt | fmt <- formats]
+  where
+    empty' = fail $ "getUpdatedTime: " ++ "could not parse time for " ++
+                    show id'
+    parseTime' = parseTime locale
+    formats =
+        [ "%a, %d %b %Y %H:%M:%S %Z"
+        , "%Y-%m-%dT%H:%M:%S%Z"
+        , "%Y-%m-%d %H:%M:%S%Z"
+        , "%Y-%m-%d"
+        , "%B %e, %Y %l:%M %p"
+        , "%B %e, %Y"
+        , "%b %d, %Y"
+        ]
